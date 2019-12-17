b7e94baa1f8d603bdb0748828e63332a1453a3bd
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-17 19:48:18 +0200

    initial commit

---
+++ Raven.FaceAuthorize/Raven.FaceAuthorize.cpp
@@ -0,0 +1,12 @@
+﻿// Raven.FaceAuthorize.cpp : Defines the entry point for the application.
+//
+
+#include "Raven.FaceAuthorize.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
