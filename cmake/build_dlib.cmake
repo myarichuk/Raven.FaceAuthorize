44edc86343e937042828762a4134c3cd4e511d4c
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-25 16:09:41 +0200

    stuff... compiles!!!

--- cmake/build_dlib.cmake
+++ cmake/build_dlib.cmake
@@ -1,6 +1,6 @@
 # This function is used to force a build on a dependant project at cmake configuration phase.
 # credit: https://stackoverflow.com/a/23570741/320103
-function (build_opencv target url)
+function (build_dlib target)
 
     set(trigger_build_dir ${CMAKE_BINARY_DIR}/${target})
 
@@ -13,53 +13,38 @@ function (build_opencv target url)
         project(${target}_project_name)
         
         #[[
-        if (MSVC OR \"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"MSVC\")
+        if (MSVC OR \"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"MSVC\")         
               foreach(flag_var
-                 CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+                 CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_DEBUG_INIT CMAKE_C_FLAGS_RELEASE
+                 CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
                  CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
                  if(${flag_var} MATCHES \"/MD\")
                     string(REGEX REPLACE \"/MD\" \"/MT\" ${flag_var} \"${${flag_var}}\")
                  endif()
-              endforeach(flag_var)          
+              endforeach(flag_var)
         endif()
-        ]]
+        #]]
+
         include(ExternalProject)
         set(CMAKE_CXX_STANDARD 17)
-
+        set(CMAKE_C_STANDARD 99)
+        
         ExternalProject_Add(${target}
-          GIT_REPOSITORY ${url}#\"http://github.com/Itseez/opencv.git\"
-          GIT_TAG \"master\"
-          SOURCE_DIR opencv
-          BINARY_DIR opencv-build
-          CMAKE_ARGS
-            -DBUILD_opencv_core=ON
-            -DBUILD_opencv_highgui=ON
-            -DBUILD_opencv_imgproc=ON
-            -DBUILD_opencv_contrib=ON
-            -DBUILD_DOCS:BOOL=FALSE
-            -DBUILD_EXAMPLES:BOOL=FALSE
-            -DBUILD_TESTS:BOOL=FALSE
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DBUILD_SHARED_LIBS:BOOL=FALSE
-            -DBUILD_NEW_PYTHON_SUPPORT:BOOL=OFF
-            -DBUILD_WITH_DEBUG_INFO=OFF
-            -DWITH_CUDA:BOOL=FALSE
-            -DWITH_FFMPEG:BOOL=FALSE
-            -DWITH_MSMF:BOOL=FALSE
-            -DWITH_IPP:BOOL=FALSE
-            -DBUILD_PERF_TESTS:BOOL=FALSE
-            -DBUILD_PNG:BOOL=ON
-            -DBUILD_JPEG:BOOL=ON
-            -DBUILD_WITH_STATIC_CRT:BOOL=OFF
-            -DBUILD_FAT_JAVA_LIB=OFF
-            -DCMAKE_INSTALL_PREFIX:PATH=${EXTERNAL_LIB_DIR}/opencv
+            GIT_REPOSITORY  \"https://github.com/davisking/dlib.git\"
+            GIT_TAG \"master\"
+            SOURCE_DIR dlib
+            CMAKE_ARGS
+            #-DDLIB_FORCE_MSVC_STATIC_RUNTIME:BOOL=ON
+            -DBUILD_SHARED_LIBS:BOOL=OFF
+            -DLIB_PNG_SUPPORT:BOOL=OFF
+            -DLIB_NO_GUI_SUPPORT:BOOL=ON
+            -DCMAKE_INSTALL_PREFIX:PATH=${EXTERNAL_LIB_DIR}/dlib           
         )
-
         add_custom_target(trigger_${target})
         add_dependencies(trigger_${target} ${target})
 
     ")
-    message("OpenCV CMAKE: ${CMAKE_LIST_CONTENT}")
+    message("dlib CMAKE: ${CMAKE_LIST_CONTENT}")
 
     file(WRITE ${trigger_build_dir}/CMakeLists.txt "${CMAKE_LIST_CONTENT}")
     execute_process(COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} ${trigger_build_dir} OUTPUT_VARIABLE stdout ERROR_VARIABLE stderr WORKING_DIRECTORY ${trigger_build_dir} )
